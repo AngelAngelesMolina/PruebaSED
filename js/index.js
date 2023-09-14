e70482a49f12f4817a53be57775d2cc341e1c704
Angel Angeles <angel.angeles.molina@gmail.com>
2023-09-14 12:36:46 -0600

    Ejemplo 2 finalizado

--- js/index.js
+++ js/index.js
@@ -9,56 +9,36 @@ const mainContent = document.querySelector('main')
 const slideContainer = document.querySelector('.slideshow-container')
 // const mainContent = document.querySelector('.main-section')
 let slideIndex = 0;
-// console.log(mainContent.classList);
-
-// document.addEventListener("DOMContentLoaded", function () {
-// console.log('Cargando contenido');
-// });
-// document.addEventListener('keydown', function (event) {
-//     if (event.key === 'Escape') {
-//         // Tu código aquí: esta función se ejecutará cuando se presione la tecla "Esc".
-//         console.log('La tecla "Esc" fue presionada.');
-//         mainContent.classList.remove('hide-element')
-//         mainContent.classList.add('show-element')
-
-//     }
-// });
-
-// Restablecer el estado predeterminado al salir de la pantalla completa
-document.addEventListener("fullscreenchange", () => {
-    if (!document.fullscreenElement) {
-        console.log('Se salió de pantalla completa');
-        document.body.style.overflow = 'visible'
-        mainContent.style.display = 'block';
-        slideContainer.style.display = 'none';
-        prueba2.style.display = 'none';
-        location.reload();
-    }
-});
+const products =
+    // Restablecer el estado predeterminado al salir de la pantalla completa
+    document.addEventListener("fullscreenchange", () => {
+        if (!document.fullscreenElement) {
+            console.log('Se salió de pantalla completa');
+            document.body.style.overflow = 'visible'
+            mainContent.style.display = 'block';
+            slideContainer.style.display = 'none';
+            prueba2.style.display = 'none';
+            location.reload();
+        }
+    });
 btnUno.addEventListener("click", () => {
-    // console.log('Click en btnOne');
-    console.log("Diste click en el boton 1")
     slideContainer.style.display = 'block'
     toggleFullScreen();
     showSlides();
 })
 
 btnDos.addEventListener("click", () => {
-    console.log("Diste click en el boton 2")
     prices.style.display = 'grid'
     toggleFullScreen()
     showSlidesTwo()
-    // showContent(false)
 });
 
 
 function toggleFullScreen() {
     if (!document.fullscreenElement) {
-        // document.requestFullscreen();
         document.documentElement.requestFullscreen();
         mainContent.style.display = 'none'
         document.body.style.overflow = 'hidden'
-        // mainContent.classList.add('hide-element')
     } else {
         if (document.exitFullscreen) {
             document.exitFullscreen();
@@ -66,45 +46,112 @@ function toggleFullScreen() {
     }
 }
 
-function showContent() {
-    prueba2.style.display = 'grid'
-
+function loadData() {
+    fetch('/js/data/lista.json')
+        .then(response => response.json())
+        .then(data => {
+            // Ahora puedes acceder a los datos del archivo JSON
+            console.log(data.limpieza);
+            return data.limpieza
+        })
+        .catch(error => {
+            console.error('Error al cargar el archivo JSON:', error);
+        });
 }
 
 function showSlides() {
-// function showSlides(isFirstPart) {
-    // var currentContainer; 
-    // if(isFirstPart){ currentContainer = 'slides' } else {currentContainer = 'galery'}
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    // console.log('Dentro show' + slideIndex);
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
     slides[slideIndex - 1].style.display = "block";
-    // clearTimeout(timeoutID);
 
-    // Establecer el nuevo setTimeout y almacenar su ID
     setTimeout(showSlides, 1000); // Cambiar imagen cada 5 segundos
 }
 function showSlidesTwo() {
-    // function showSlides(isFirstPart) {
-        // var currentContainer; 
-        // if(isFirstPart){ currentContainer = 'slides' } else {currentContainer = 'galery'}
-        for (let i = 0; i < slidesTwo.length; i++) {
-            slidesTwo[i].style.display = "none";
-        }
-        slideIndex++;
-        // console.log('Dentro show' + slideIndex);
-        if (slideIndex > slidesTwo.length) {
-            slideIndex = 1;
-        }
-        slidesTwo[slideIndex - 1].style.display = "block";
-        // clearTimeout(timeoutID);
-    
-        // Establecer el nuevo setTimeout y almacenar su ID
-        setTimeout(showSlidesTwo, 1000); // Cambiar imagen cada 5 segundos
+    for (let i = 0; i < slidesTwo.length; i++) {
+        slidesTwo[i].style.display = "none";
     }
-// showContent(true)
+    slideIndex++;
+    if (slideIndex > slidesTwo.length) {
+        slideIndex = 1;
+    }
+    slidesTwo[slideIndex - 1].style.display = "block";
+    fetch('/js/data/lista.json')
+        .then(response => response.json())
+        .then(data => {
+            const limpieza = data.limpieza;
+
+            // Obtener el elemento contenedor
+            const contenedor = document.getElementById('contenedor');
+            // contenedor.style.margin = '30px'
+
+            const hojaDiv = document.createElement('div');
+            hojaDiv.classList.add('hoja');
+            const hoja = limpieza[slideIndex - 1].hoja;
+
+            for (let j = 0; j < hoja.length; j++) {
+                const item = hoja[j];
+                const productoDiv = document.createElement('div');
+                productoDiv.classList.add('producto');
+
+                // Crea dos elementos de párrafo
+                const productoParrafo = document.createElement('p');
+                const precioParrafo = document.createElement('p');
+
+                // Establece el contenido de los párrafos
+                if (item.producto != '' && item.precio != '') {
+                    productoParrafo.innerHTML = `<strong>Producto:</strong> ${item.producto}`;
+                    precioParrafo.innerHTML = `<strong>Precio:</strong> ${item.precio}`;
+                }
+
+                // Agrega los párrafos al div productoDiv
+                productoDiv.appendChild(productoParrafo);
+                productoDiv.appendChild(precioParrafo);
+
+                hojaDiv.appendChild(productoDiv);
+            }
+            contenedor.innerHTML = ''
+            contenedor.appendChild(hojaDiv);
+
+            // Recorrer el arreglo "limpieza" y crear elementos HTML para cada objeto
+            //TODO
+            // limpieza.forEach(hoja => {
+            //     const hojaDiv = document.createElement('div');
+            //     hojaDiv.classList.add('hoja');
+
+            //     hoja.hoja.forEach(item => {
+            //         const productoDiv = document.createElement('div');
+            //         productoDiv.classList.add('producto');
+            //         productoDiv.innerHTML = `<strong>Producto:</strong> ${item.producto}, <strong>Precio:</strong> ${item.precio}`;
+            //         hojaDiv.appendChild(productoDiv);
+            //     });
+
+            //     contenedor.appendChild(hojaDiv);
+            // });
+            // for (let i = 0; i < limpieza.length; i++) {
+            //     const hojaDiv = document.createElement('div');
+            //     hojaDiv.classList.add('hoja');
+            //     const hoja = limpieza[i].hoja;
+
+            //     for (let j = 0; j < hoja.length; j++) {
+            //         const item = hoja[j];
+            //         const productoDiv = document.createElement('div');
+            //         productoDiv.classList.add('producto');
+            //         productoDiv.innerHTML = `<strong>Producto:</strong> ${item.producto}, <strong>Precio:</strong> ${item.precio}`;
+            //         hojaDiv.appendChild(productoDiv);
+            //     }
+
+            //     contenedor.appendChild(hojaDiv);
+
+            // }
+            // return data.limpieza
+        })
+        .catch(error => {
+            console.error('Error al cargar el archivo JSON:', error);
+        });
+    setTimeout(showSlidesTwo, 1000); // Cambiar imagen cada 5 segundos
+}
